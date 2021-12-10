63ba166f98c2cd4f40dc0d853f9558300c41a9b9
Abdulhafeez012 <aboodhammed92@gmail.com>
2021-12-10 16:12:10 +0200

    JS, finishing from the project

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 let cards = document.getElementsByClassName("card");
 let btn = document.getElementsByClassName("card-modal-form");
 let Number_element = document.getElementsByClassName("card");
-let totalItem = document.querySelector(".total-items-badge");
-let totalAmount = document.querySelector(".cart-total-amount");
+let totalItemBtn = document.querySelector(".total-items-badge");
+let totalAmountBtn = document.querySelector(".cart-total-amount");
+let CardTitle = document.getElementsByClassName("card-title");
+let totalItemCart = document.getElementById("total_items");
+var itemArray = [];
 var counter = 0;
 var price = 0;
 var processPrice = 0;
+
+// addtion function
+function addtion(ele){
+    counter++;
+    price = +document.getElementById(`MainPrice_${ele}`).textContent;
+    processPrice += price;
+    itemArray.push(CardTitle.item(--ele).textContent);
+};
+//delete function
+function del(ele){
+    counter--;
+    price = +document.getElementById(`MainPrice_${ele}`).textContent;
+    counter === 0?processPrice = 0:processPrice -= price;
+    itemArray.pop(CardTitle.item(--ele).textContent);
+}
+
 for(let i = 1 ; i <= Number_element.length; i++){
-    document.getElementById(`card_btn_${i}`).onclick = function (){
-        counter++;
-        price = +document.getElementById(`MainPrice_${i}`).textContent;
-        processPrice += price;
-        totalItem.innerHTML = counter;
-        totalAmount.innerHTML = processPrice.toFixed(2);
+    //add to cart functionality
+    document.getElementById(`card_btn_${i}`).onclick = function(){
+        addtion(i);
+        totalItemBtn.innerHTML = counter;
+        totalItemCart.value = counter;
+        totalAmountBtn.innerHTML = `$${processPrice.toFixed(2)}`;
+        document.querySelector("#total_amount").innerHTML = `$${processPrice.toFixed(2)}`;
+        document.querySelector("#name_item").innerHTML = `${itemArray} `; 
+        //+ button functionality 
+        document.getElementById("add_btn").onclick = function(){
+                console.log(i);
+                addtion(i);
+                totalItemBtn.innerHTML = counter;
+                totalItemCart.value = counter;
+                totalAmountBtn.innerHTML = `$${processPrice.toFixed(2)}`;
+                document.querySelector("#total_amount").innerHTML = `$${processPrice.toFixed(2)}`;
+                document.querySelector("#name_item").innerHTML = `${itemArray} `;   
+        };
+        //- button functionality
+        document.getElementById("del_btn").onclick = function(){
+                if(totalItemCart.value>0){
+                    console.log(i);
+                    del(i);
+                    totalItemBtn.innerHTML = counter;
+                    totalItemCart.value = counter;
+                    totalAmountBtn.innerHTML = `$${processPrice.toFixed(2)}`;
+                    document.querySelector("#total_amount").innerHTML = `$${processPrice.toFixed(2)}`;
+                    document.querySelector("#name_item").innerHTML = `${itemArray}`;
+                };
+        };
     };
 }
+
